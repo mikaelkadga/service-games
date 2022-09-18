f120a062b7ca29cd90b9cb6f4f29daa7de155242
rijalrubio <rijalrubio@gmail.com>
2022-09-19 05:20:18 +0700

    user profile first commit

--- src/user/user.service.js
+++ src/user/user.service.js
@@ -1,20 +1,38 @@
 const userRepo = require("./user.repository");
 const bcrypt = require("bcrypt");
-const saltRound =10;
+const saltRound = 10;
 
-const createUser = async ({ username, fullname, email, password}) => {
-    const hashPassword = await bcrypt.hash(password, saltRound);
-    
-    return await userRepo.createUser({
-        username,
-        fullname,
-        email,
-        password: hashPassword,
-    });
+const createUser = async ({ username, fullname, email, password }) => {
+  const hashPassword = await bcrypt.hash(password, saltRound);
+
+  return await userRepo.createUser({
+    username,
+    fullname,
+    email,
+    password: hashPassword,
+  });
+};
+
+const getUserProfile = async ({ userId }) => {
+  return await userRepo.getUserProfile({
+    userId,
+  });
+};
+
+const updateUser = async ({ fullName, email, password }) => {
+  const hashPassword = await bcrypt.hash(password, saltRound);
+  return await userRepo.updateUser({
+    userId,
+    fullName,
+    email,
+    password: hashPassword,
+  });
 };
 
 const userService = {
-    createUser
-}
+  createUser,
+  getUserProfile,
+  updateUser,
+};
 
 module.exports = userService;
