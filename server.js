2170bc92d4aa19eb91b6e1bb1ec394ee548778d9
ashidiqidimas <prabowo6@gmail.com>
2022-09-20 18:36:01 +0700

    added api-docs

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const PORT = process.env.PORT || 8000
 
 app.use(express.urlencoded({ extended: false }))
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./src/config/swagger');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.set('view engine', 'ejs')
 const userRouter = require("./src/user/user.route");
 
--- src/docs/docs.route.js
+++
@@ -1,8 +0,0 @@
-const docsRouter = require("express").Router();
-const swaggerUi = require("swagger-ui-express");
-const swaggerDocument = require("./swagger");
-
-docsRouter.use("/api-docs", swaggerUi.serve);
-docsRouter.get("/api-docs", swaggerUi.setup(swaggerDocument));
-
-// export default docsRouter;
