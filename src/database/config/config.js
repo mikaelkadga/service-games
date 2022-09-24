24bc83dd8693e6c4fc8b5e61c8baaa620886d258
Alief <aliefiayasin@gmail.com>
2022-09-24 10:52:21 +0200

    revert config change

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,11 +2,10 @@ require("dotenv").config();
 
 const config = {
   development: {
-    username: "alief",
-    password: "123456",
-    database: "chapter9",
-    host:  "127.0.0.1",
-    port: "5432",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     dialect: "postgres",
   },
 };
