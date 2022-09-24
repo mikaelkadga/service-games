c05bbb27729b5cf33f625039a1a610a4f6e674de
mikaelkadga <mikaelkadga@gmail.com>
2022-09-24 20:48:03 +0700

    fix-leaderboard

--- src/leaderboard/leaderboard.route.js
+++ src/leaderboard/leaderboard.route.js
@@ -7,35 +7,34 @@ leaderboardRouter.get("/leaderboard", tokenVerification, leaderboardController.g
 
 /**
  * @swagger
- * /leaderboard:
- *  get:
- *    tags:
- *      - Leaderboard
- *    summary: user total point
- *    description: API untuk mengambil total point user
- *    responses:
- *      '200':
- *        content:
- *          application/json:
- *            schema:
- *              type: object
- *              properties:
- *                fullname:
- *                  type: string
- *                  example: Rijal Rubi
-                  totalPoint:
-                    type: integer
-                    example: 123123
-                  userId: 
-                    type: integer
-                    exmample: 1
- *      '500':
- *        content:
- *         application/json:
- *           schema:
- *            type: object
- *            properties:
- *              message:
+ *  /leaderboard:
+ *    post:
+ *      security:
+ *        - bearerAuth: []
+ *      summary: Create a room
+ *      description: this api is used to get user total point 
+ *      tags:
+ *        - Leaderboard
+ *      responses:
+ *        '200':
+ *          description: 
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: string
+ *                example: JSKDN
+ *        '400':
+ *          description: Missing Authorization header
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: string
+ *                example: Missing Authorization header
+ *        '500':
+ *          description: Internal Server Error
+ *          content:
+ *            application/json:
+ *              schema:
  *                type: string
  *                example: Internal Server Error
  */
