f26e6f1b4193c9ebbaa080bac4a4750556322a23
Alief <aliefiayasin@gmail.com>
2022-09-26 14:19:18 +0200

    fix get user

--- src/user/user.controller.js
+++ src/user/user.controller.js
@@ -17,13 +17,15 @@ const createUser = async (req, res) => {
 };
 
 const getUserProfile = async (req, res) => {
+  console.log("test1");
   const userId = req.auth.id;
   try {
-    const getUserProfile = await userService.getUser({
+    const getUserProfile = await userService.getUserProfile({
       userId,
     });
     return res.status(200).json(getUserProfile);
   } catch (error) {
+    console.log(error);
     return res.status(500).json({ message: "Internal server error!" });
   }
 };
