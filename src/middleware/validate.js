7e0a9072054423181d9fd9e29a70b0e03f94dcad
Alief <aliefiayasin@gmail.com>
2022-10-09 21:57:15 +0200

    fix double error

--- src/middleware/validate.js
+++ src/middleware/validate.js
@@ -2,14 +2,13 @@ const { validationResult } = require("express-validator");
 const validate = (validations) => {
   return async (req, res, next) => {
     await Promise.all(validations.map((validation) => validation.run(req)));
-
     const errors = validationResult(req);
     if (errors.isEmpty()) {
       return next();
     }
 
     res.status(422).json({
-      errors: errors.array(),
+      errors: errors.array({ onlyFirstError: true }),
     });
   };
 };
