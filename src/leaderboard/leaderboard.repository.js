35a3f9fc0e5e7bf47f4bcc4bd66d79db92820929
gayohignasius <gayohignasius>
2022-10-17 15:50:08 +0700

    fixing leaderboard

--- src/leaderboard/leaderboard.repository.js
+++ src/leaderboard/leaderboard.repository.js
@@ -1,12 +1,17 @@
-const { User } = require("../database/models");
+const { User } = require('../database/models');
+const { Op } = require('sequelize');
 
 const getAllUser = async () => {
   return await User.findAll({
-    attributes: ["fullname", "totalPoint", "userId"],
-    order: [
-      ["totalPoint", "DESC"],
-    ],
-    limit: 5,
+    where: {
+      totalPoint: {
+        [Op.not]: null,
+        [Op.gt]: 0,
+      },
+    },
+    attributes: ['fullname', 'totalPoint', 'userId'],
+    order: [['totalPoint', 'DESC']],
+    limit: 10,
   });
 };
 
